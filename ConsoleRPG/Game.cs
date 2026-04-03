98f8e5fbd93d22a76b68b17828d2c97b9387b1cc
Woodsleader <woodsleader@hotmail.com>
2026-04-04 00:33:22 +0200

    Creation of new methods and tweaks

--- ConsoleRPG/Game.cs
+++ ConsoleRPG/Game.cs
@@ -8,16 +8,65 @@ namespace ConsoleRPG
     {
         public static void GameRunning()
         {
+            var Player = new Hero();
+            AskHeroName();
             bool isRunning = true;
             while (isRunning)
             {
-
+                MainMenu();
             }
         }
 
-        public static void MainMenu()
+        private static void MainMenu()
         {
-            Console.WriteLine("1 - Character \n 2 - Shop \n 3 - Tavern \n 4 - Dungeon");
+            Console.WriteLine("1 - Character \n2 - Shop \n3 - Tavern \n4 - Dungeon");
+            switch (UserChoice(1, 4))
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
+        private static void CharacterMenu()
+        {
+            Console.WriteLine("1 - Equip items \n2 - Unequip items \n 3 - View equipped items \n 4 - Character stats \n 5 - Leave");
+            switch (UserChoice(1, 5))
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        }
+
+        //Name and choices check.
+        public static int UserChoice(int min, int max)
+        {
+            while (true)
+            {
+                Console.WriteLine($"Select a number between {min} and {max},");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int choice) && choice >= min && choice <= max)
+                {
+                    return choice;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect number, try again");
+                }
+            }
         }
 
         public static string AskHeroName()
